e8f9ae05155f3b6c52b22aabeb189e6ca094d78a
Sairam595 <sairam2279@yahoo.com>
2025-06-17 23:47:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import InlineComponent from "./components/InlineComponent";
+// import OutlineComponent from "./components/OutlineComponent";
+
+import Header from "./components/Header";
+import Todo from "./components/Todo";
+import "./components/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Todo />
+      {/* <InlineComponent />
+      <OutlineComponent /> */}
+    </div>
+  );
+}
+
+export default App;
